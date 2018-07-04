333bbe6283dc0b00a4e9bb8d47f2cefabc5fd51e
Britney Smith <brit.smith7@gmail.com>
2018-07-03 23:29:12 -0400

    Completed dictionary challenge

--- code challenges/cs/PhoneDict/Program.cs
+++ code challenges/cs/PhoneDict/Program.cs
@@ -8,14 +8,14 @@ namespace PhoneDict
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Number of entries: ");
+            //Console.WriteLine("Number of entries: ");
             int numEntries = Convert.ToInt32(Console.ReadLine());
             IDictionary<string, int> phoneBook = new Dictionary<string, int>();
             
 
             for (int i = 0; i < numEntries; i++)
             {
-                Console.WriteLine("Entries: ");
+                //Console.WriteLine("Entries: ");
                 var entry = Console.ReadLine();
                 string[] tempArray = entry.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
                 phoneBook.Add(new KeyValuePair<string, int>(tempArray[0], Convert.ToInt32(tempArray[1])));
@@ -24,10 +24,29 @@ namespace PhoneDict
             /* phoneBook.Add(new KeyValuePair<string, int>("sam", 8675309));
             phoneBook.Add(new KeyValuePair<string, int>("sue", 1111111));*/
 
-            foreach (var entry in phoneBook)
+            /* foreach (var entry in phoneBook)
             {
                 Console.WriteLine(entry);
+            } */
+
+            string search = "";
+            List<string> searchNames = new List<string>();
+            while(!string.IsNullOrEmpty(search = Console.ReadLine()))
+            {
+                searchNames.Add(search);   
             }
-        }
+            int numValue;
+            for (int j = 0; j < searchNames.Count; j++)
+            {
+                if (phoneBook.TryGetValue(searchNames[j], out numValue))
+                {
+                    Console.WriteLine("{0}={1}", searchNames[j], numValue);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }    
+            } 
+        }  
     }
 }
