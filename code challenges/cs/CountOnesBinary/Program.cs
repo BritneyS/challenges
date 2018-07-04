17ea69ede5db9663d189fe2e775a9f3ef1020723
Britney Smith <brit.smith7@gmail.com>
2018-07-04 13:12:58 -0400

    CountOnesBinary counts total 1s

---
+++ code challenges/cs/CountOnesBinary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountOnesBinary
+{
+    class Program
+    {
+        public static List<int> remainderList = new List<int>();
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            
+            while (divideNum(n) > 0)
+            {
+                divideNum(n);
+            }
+
+            Console.WriteLine(getOnesCount());
+            
+        }
+
+        static int divideNum (int num)
+        {
+            //Console.WriteLine("Num: " + num);
+            if (num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                setRemainderList(num % 2);
+                return num = divideNum(num / 2);
+            }
+        
+        }
+
+        static void setRemainderList (int num2)
+        {
+            remainderList.Insert(0, num2);
+        }
+
+        static int getOnesCount ()
+        {
+            int countOnes = 0;
+
+            foreach (var digit in remainderList)
+            {
+                
+                if (digit == 1)
+                {
+                    countOnes++;
+                }
+                else 
+                {
+                    countOnes = countOnes + 0;
+                }
+                
+            }
+
+            return countOnes;
+            
+        }
+    }
+}
